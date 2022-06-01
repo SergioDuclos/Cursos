d22c3ade1e9a96c0f22b5ae5f6d0187556192ffa
Sergio Duclos <sergioduclos@gmail.com>
2022-06-01 13:09:44 -0300

    Update functions.js

--- rocketseat/js/functions.js
+++ rocketseat/js/functions.js
@@ -1,1 +1,12 @@
 console.log ("Aula sobre funções");
+
+
+function createPhrases(){
+console.log (" Estudar é muito bom");
+console.log (" Paciência e Persistência");
+console.log (" Revisão é a mãe do aprendizado"); 
+}
+
+createPhrases()
+createPhrases()
+createPhrases()
