f0b1c49aa84c98256c968fb655e1d7ba8a8ead73
Sergio Duclos <sergioduclos@gmail.com>
2022-06-01 11:00:48 -0300

    Update script.js

--- rocketseat/js/script.js
+++ rocketseat/js/script.js
@@ -2,3 +2,12 @@ console.log ("Teste");
 console.log (12.5 + 33);
 console.log (Infinity);
 console.log (undefined);
+
+console.log ({
+    name:"Sergio",
+    idade: 53,
+    andar: function(){
+        console.log ("Andar");
+    } 
+
+});
