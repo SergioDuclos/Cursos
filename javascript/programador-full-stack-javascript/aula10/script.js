d149e3829f91490ceddb64628a9858f683956cec
Sergio Duclos <sergioduclos@gmail.com>
2022-06-24 12:23:38 -0300

    update

--- javascript/programador-full-stack-javascript/aula10/script.js
+++ javascript/programador-full-stack-javascript/aula10/script.js
@@ -1,3 +1,25 @@
+/*
+<!-- //let spaceship = "FE Supernova"
+//console.log(spaceship);
+//spaceship = "FE Helmet"
+//console.log(spaceship);
+
+//let noValue
+//console.log(noValue);
+let departureConfirmation = confirm("Nave Pronta")
+console.log (departureConfirmation)
+
+ alert ("Bem Vindo")
+let name = prompt ("Qual o seu nome")
+let age = prompt ("Informe sua idade")  
+let ageConfirmation = confirm ("Sua idade " +age+ " anos") 
+alert ("Nome: "+ name)
+alert ("Idade: "+ age)
+alert ("Idade Confirmada "+ ageConfirmation)
+
+-->
+
+
 
 let pessoaVelha = prompt ("Digite o Nome da Pessoa mais Velha ")
 let idadeVelha = prompt ("Digite o Nome da Pessoa mais Velha ")
@@ -9,3 +31,16 @@ alert ("Diferença de idade é : " + idadeVelha -idadeNova)
 
 alert ( "Nome " + pessoaVelha + " Idade " + idadeVelha )
 alert ( "Nome " + pessoaNova + " Idade " + idadeNova )
+
+
+*/
+
+let velocity = 110
+if (velocity < 100){
+    alert ("Estamos numa velocidade aceitável")
+
+}else{
+    alert ("Estamos numa velocidade de risco")
+}
+
+
