4ad30c4c24a39051f403025a65db9195b527d8ef
Sergio Duclos <sergioduclos@gmail.com>
2022-06-24 10:21:19 -0300

    Update Exercicio.js

--- javascript/programador-full-stack-javascript/Exercicio.js
+++ javascript/programador-full-stack-javascript/Exercicio.js
@@ -1,5 +1,8 @@
 
-let spaceship = "FE Supernova"
-console.log(spaceship);
-spaceship = "FE Helmet"
-console.log(spaceship);
+//let spaceship = "FE Supernova"
+//console.log(spaceship);
+//spaceship = "FE Helmet"
+//console.log(spaceship);
+
+let noValue
+console.log(noValue);
