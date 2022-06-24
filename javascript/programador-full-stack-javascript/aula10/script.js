316abf67cf1a1afb0a5ba82c4c3c8640a1d0ee9d
Sergio Duclos <sergioduclos@gmail.com>
2022-06-24 11:17:30 -0300

    update

---
+++ javascript/programador-full-stack-javascript/aula10/script.js
@@ -0,0 +1,11 @@
+
+let pessoaVelha = prompt ("Digite o Nome da Pessoa mais Velha ")
+let idadeVelha = prompt ("Digite o Nome da Pessoa mais Velha ")
+
+let pessoaNova = prompt ("Digite o Nome da Pessoa mais Nova ")
+let idadeNova = prompt ("Digite o Nome da Pessoa mais Nova ")
+
+alert ("Diferença de idade é : " + idadeVelha -idadeNova)
+
+alert ( "Nome " + pessoaVelha + " Idade " + idadeVelha )
+alert ( "Nome " + pessoaNova + " Idade " + idadeNova )
