ae198cc463957870176f76df2d37bbc18b6f8cdf
Sergio Duclos <sergioduclos@gmail.com>
2022-06-24 14:46:29 -0300

    Update script.js

--- javascript/programador-full-stack-javascript/aula19/script.js
+++ javascript/programador-full-stack-javascript/aula19/script.js
@@ -3,7 +3,7 @@ let departureDateEntray =  prompt (" Digite a data de partida (Formato da data D
 let departureDate = moment (departureDateEntray, "DD/MM/YYYY");
 let today = moment ();
 let dateDiff = today - departureDate;
-let chosenOption = prompt ("Escolha como gostaria de exibir o tempo de partida \n1- Segundos\n2- Minutos\n3- Horas\n4- Dias  ");
+let chosenOption = prompt("Escolha como gostaria de exibir o tempo de partida \n1- Segundos\n2- Minutos\n3- Horas\n4- Dias  ");
    if (chosenOption == "1"){
       let secondsOfDeparture = Math.round(dateDiff/1000)
       alert ("Tempo de Vôo: " + secondsOfDeparture + " segundos");
