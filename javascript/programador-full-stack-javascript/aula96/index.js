e5e2a5960f04236c4021076f8ffd90562a9ec50f
Sergio Duclos <sergioduclos@gmail.com>
2022-06-27 12:33:14 -0300

    Update index.js

--- javascript/programador-full-stack-javascript/aula96/index.js
+++ javascript/programador-full-stack-javascript/aula96/index.js
@@ -1,1 +1,5 @@
 let spaceship = prompt ("Digite o nome da nave")
+let charToReplace = prompt ("Qual caracter deseja substituir?")
+let replacementChar = prompt (" Por qual caracter você deseja substituir?")
+
+console.log (spaceship, charToReplace, replacementChar)
